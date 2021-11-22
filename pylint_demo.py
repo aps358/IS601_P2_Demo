fccc0284692eb3b9240c88b0162e7119d0c3fc91
aps358 <as358@njit.edu>
2021-11-21 20:14:00 -0500

    Created demo file

---
+++ pylint_demo.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def addition(self, num1, num2):
+        return num1 + num2
+
+    def subtraction(self, num1, num2):
+        return num1 - num2
+
+    def multiplication(self, num1, num2):
+        return num1 * num2
+
+    def division(self, num1, num2):
+        return num1 / num2
