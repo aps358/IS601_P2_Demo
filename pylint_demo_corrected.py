306df4d77d8147545fb59a8b5f79eb6de314b6dc
aps358 <as358@njit.edu>
2021-11-21 20:55:43 -0500

    Created pylint demo correction

--- pylint_demo_corrected.py
+++ pylint_demo_corrected.py
@@ -1,19 +1,27 @@
-class Calculator:
 
+""" Contains the class Calculator """
+
+
+class Calculator:
+    """ Defining the methods of Calculator in here """
     answer = 0
 
     def addition(self, num1, num2):
+        """ Adds the given two numbers and returns the answer """
         self.answer = num1 + num2
         return self.answer
 
     def subtraction(self, num1, num2):
+        """ Subtracts the given two numbers and returns the answer """
         self.answer = num1 - num2
         return self.answer
 
     def multiplication(self, num1, num2):
+        """ Multiplies the given two numbers and returns the answer """
         self.answer = num1 * num2
         return self.answer
 
     def division(self, num1, num2):
+        """ Divides the given two numbers and returns the answer """
         self.answer = num1 / num2
         return self.answer
